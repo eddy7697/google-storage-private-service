8e032da55641e9b5b2af36988c7c4e24b531bd77
Eddy <vincent7697@gmail.com>
2018-07-24 23:32:07 +0800

    modify

--- routes/index.js
+++ routes/index.js
@@ -14,25 +14,21 @@ const bucketName = '123456789';
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const filePath = './uploads';
-
+  const bucketName = 'nearlinetest-mark';
   const images = fs.readdirSync(filePath);
 
   console.log(images);
 
   storage
-    .bucket('nearlinetest-mark')
-    .upload('/usr/local/google-storage-sample/uploads/000002.jpg', {
-      // Support for HTTP requests made with `Accept-Encoding: gzip`
-      gzip: true,
-      metadata: {
-        // Enable long-lived HTTP caching headers
-        // Use only if the contents of the file will never change
-        // (If the contents will change, use cacheControl: 'no-cache')
-        cacheControl: 'public, max-age=31536000',
-      },
-    })
-    .then(() => {
-      console.log(`${filename} uploaded to ${bucketName}.`);
+    .bucket(bucketName)
+    .getFiles()
+    .then(results => {
+      const files = results[0];
+
+      console.log('Files:');
+      files.forEach(file => {
+        console.log(file);
+      });
     })
     .catch(err => {
       console.error('ERROR:', err);
