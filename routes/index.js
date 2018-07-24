2103403078c36eb52712576122228647fbce56aa
Eddy <vincent7697@gmail.com>
2018-07-24 22:07:50 +0800

    modify

--- routes/index.js
+++ routes/index.js
@@ -19,17 +19,42 @@ router.get('/', function(req, res, next) {
 
   console.log(images);
 
-  storage.createBucket(bucketName)
-         .then((res) => {
-            // console.log(res)
-         }).catch(err => {
-            // console.error('error', err);
-         });
-         
   storage.getBuckets(function(err, buckets) {
     if (!err) {
       // buckets is an array of Bucket objects.
     }
+  });
+  
+  //-
+  // To control how many API requests are made and page through the results
+  // manually, set `autoPaginate` to `false`.
+  //-
+  var callback = function(err, buckets, nextQuery, apiResponse) {
+    if (nextQuery) {
+      // More results exist.
+      console.log(buckets)
+      storage.getBuckets(nextQuery, callback);
+    }
+  
+    // The `metadata` property is populated for you with the metadata at the
+    // time of fetching.
+    buckets[0].metadata;
+  
+    // However, in cases where you are concerned the metadata could have
+    // changed, use the `getMetadata` method.
+    buckets[0].getMetadata(function(err, metadata, apiResponse) {});
+  };
+  
+  storage.getBuckets({
+    autoPaginate: false
+  }, callback);
+  
+  //-
+  // If the callback is omitted, we'll return a Promise.
+  //-
+  storage.getBuckets().then(function(data) {
+    var buckets = data[0];
+
     console.log(buckets)
   });
 
