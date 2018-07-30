c4bc54ca88c4195f8143090921216eafeaa0cfbc
Eddy <vincent7697@gmail.com>
2018-07-30 22:22:54 +0800

    VISION API

--- routes/vision.js
+++ routes/vision.js
@@ -11,10 +11,10 @@ var bucketName = 'nearlinetest-mark';
 router.get('/:img', function(req, res, next) {
 'use strict';
 
-console.log(req.params.taskId);
+console.log(req.params.img);
 
 client
-  .labelDetection(`gs://${bucketName}/resources/city.jpg`)
+  .labelDetection(`gs://${bucketName}/${req.params.img}`)
   .then(results => {
     const labels = results[0].labelAnnotations;
 
