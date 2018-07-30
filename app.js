2d62794bdd5073718b0c4ca0edf489bd7c0015a4
Eddy <vincent7697@gmail.com>
2018-07-30 22:38:35 +0800

    VISION API

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var uploadRouter = require('./routes/upload');
 var visionRouter = require('./routes/vision');
+var fileRouter = require('./routes/file');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/vision', visionRouter);
+app.use('/file', fileRouter);
 app.use('/upload/image', uploadRouter);
 
 // catch 404 and forward to error handler
