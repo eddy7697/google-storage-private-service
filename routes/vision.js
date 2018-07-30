d4ef878039c0a94a72a4370479a8ebf832f99068
Eddy <vincent7697@gmail.com>
2018-07-30 21:54:22 +0800

    VISION API

--- routes/vision.js
+++ routes/vision.js
@@ -1,26 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var vision = require('@google-cloud/vision');
+const vision = require('@google-cloud/vision');
 
 const client = new vision.ImageAnnotatorClient();
+
 var bucketName = 'nearlinetest-mark';
 var fileName = '000002.jpg';
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  client
-    .landmarkDetection(`gs://${bucketName}/${fileName}`)
-    .then(result => {
-        
-        console.log('Detect success', result)
-    })
-    .catch(err => {
-        console.log('Detect fail....', err)
-    })
-    
-  res.send('respond with a resource');
+'use strict';
 
+client
+  .labelDetection('gs://${bucketName}/resources/city.jpg')
+  .then(results => {
+    const labels = results[0].labelAnnotations;
+
+    console.log(labels);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
+// [END vision_quickstart]
   
 });
 
