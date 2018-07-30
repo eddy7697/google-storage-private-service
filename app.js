73056db1f906bd199b076072719485d90bd13939
Eddy <vincent7697@gmail.com>
2018-07-30 23:40:04 +0800

    f2e

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ var usersRouter = require('./routes/users');
 var uploadRouter = require('./routes/upload');
 var visionRouter = require('./routes/vision');
 var metaRouter = require('./routes/meta');
+var filesRouter = require('./routes/files');
 
 var app = express();
 
 // view engine setup
+app.engine('ejs', require('express-ejs-extend'));
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -33,6 +35,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/vision', visionRouter);
 app.use('/meta', metaRouter);
+app.use('/files', filesRouter);
 app.use('/upload/image', uploadRouter);
 
 // catch 404 and forward to error handler
---
+++ routes/files.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const Storage = require('@google-cloud/storage');
+const fs = require('fs');
+const router = express.Router();
+const projectId = 'tonal-bank-198910';
+const storage = new Storage({
+  projectId: projectId
+})
+
+const bucketName = 'nearlinetest-mark';
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const filePath = './uploads';  
+  const images = fs.readdirSync(filePath);  
+
+  const getFilesPromise = new Promise(function(resolve, reject) {
+      storage
+        .bucket(bucketName)
+        .getFiles()
+        .then(results => {
+          const files = results[0];
+
+          resolve(files)
+        })
+        .catch(err => {
+          reject(err)
+        });      
+    });
+  
+  const uploadFilesToStorage = new Promise((resolve, reject) => {
+    if (images.length > 0) {
+      var fileName = filePath + '/' + images[0];
+
+      storage
+        .bucket(bucketName)
+        .upload(fileName, {
+          gzip: true,
+          metadata: {
+            cacheControl: 'public, max-age=31536000',
+          },
+        })
+        .then(() => {
+          fs.unlinkSync(fileName);
+          storage
+            .bucket(bucketName)
+            .getFiles()
+            .then(results => {
+              const files = results[0];
+    
+              resolve(files)
+            })
+            .catch(err => {
+              reject(err)
+            }); 
+        })
+        .catch(err => {
+          reject(err)
+          console.error('ERROR:', err);
+        });
+    } else {
+      storage
+        .bucket(bucketName)
+        .getFiles()
+        .then(results => {
+          const files = results[0];
+
+          resolve(files)
+        })
+        .catch(err => {
+          reject(err)
+        });  
+    }
+    
+  })
+  
+  Promise.all([
+    uploadFilesToStorage
+  ]).then((results) => {
+    res.send(results[0]);
+    res.render('index', { 
+      title: 'Express',
+      images: images ,
+      storageFiles: results[0]
+    });
+  }, (err) => {
+    res.send([]);
+  });
+  
+});
+
+function uploadFile(bucketName, filename) {
+  const Storage = require('@google-cloud/storage');
+  const storage = new Storage();
+  
+  storage
+    .bucket(bucketName)
+    .upload(filename, {
+      gzip: true,
+      metadata: {
+        cacheControl: 'public, max-age=31536000',
+      },
+    })
+    .then(() => {
+      console.log(`${filename} uploaded to ${bucketName}.`);
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+  // [END storage_upload_file]
+}
+
+module.exports = router;
