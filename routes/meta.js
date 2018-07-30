cc8945e1c0aa40b6d09161081aad95741176f152
Eddy <vincent7697@gmail.com>
2018-07-30 22:45:49 +0800

    VISION API

--- routes/meta.js
+++ routes/meta.js
@@ -14,8 +14,6 @@ const storage = new Storage({
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-    console.log(req.query.name);
-
     storage
         .bucket(bucketName)
         .file(req.query.name)
@@ -23,27 +21,7 @@ router.get('/', function(req, res, next) {
         .then(results => {
         const metadata = results[0];
 
-        console.log(`File: ${metadata.name}`);
-        console.log(`Bucket: ${metadata.bucket}`);
-        console.log(`Storage class: ${metadata.storageClass}`);
-        console.log(`Self link: ${metadata.selfLink}`);
-        console.log(`ID: ${metadata.id}`);
-        console.log(`Size: ${metadata.size}`);
-        console.log(`Updated: ${metadata.updated}`);
-        console.log(`Generation: ${metadata.generation}`);
-        console.log(`Metageneration: ${metadata.metageneration}`);
-        console.log(`Etag: ${metadata.etag}`);
-        console.log(`Owner: ${metadata.owner}`);
-        console.log(`Component count: ${metadata.component_count}`);
-        console.log(`Crc32c: ${metadata.crc32c}`);
-        console.log(`md5Hash: ${metadata.md5Hash}`);
-        console.log(`Cache-control: ${metadata.cacheControl}`);
-        console.log(`Content-type: ${metadata.contentType}`);
-        console.log(`Content-disposition: ${metadata.contentDisposition}`);
-        console.log(`Content-encoding: ${metadata.contentEncoding}`);
-        console.log(`Content-language: ${metadata.contentLanguage}`);
-        console.log(`Metadata: ${metadata.metadata}`);
-        console.log(`Media link: ${metadata.mediaLink}`);
+        res.send(metadata);
         })
         .catch(err => {
         console.error('ERROR:', err);
