40d013e0f15ecd48be1b00875da6e6c56d7eff36
Eddy <vincent7697@gmail.com>
2018-07-30 20:35:28 +0800

    vision route

---
+++ routes/vision.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var vision = require('@google-cloud/vision');
+
+const client = new vision.ImageAnnotatorClient();
+var bucketName = 'nearlinetest-mark';
+var fileName = '000002.jpg';
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  client
+    .faceDetection(`gs://${bucketName}/${fileName}`)
+    .then(result => {
+        console.log(result)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+    
+  res.send('respond with a resource');
+
+  
+});
+
+module.exports = router;
