cda2dc9a0f0f3ec5429a919bab0e9aba84556ffe
李茗浤 <vincent7697@gmail.com>
2018-11-16 16:09:50 +0800

    update

--- routes/prediction.js
+++ routes/prediction.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const { exec } = require('child_process')
 const automl = require('@google-cloud/automl');
 
 const router = express.Router();
@@ -14,22 +15,36 @@ const bucketName = 'nearlinetest-mark';
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var client = new automl.v1beta1.PredictionServiceClient();
-    var formattedName = client.modelPath(projectId, location, modelId);
-    var payload = fs.readFileSync('./public/uploads/request.json')
-    var request = {
-      name: formattedName,
-      payload: payload,
-    };
+    exec(`curl -X POST -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+        https://automl.googleapis.com/v1beta1/projects/tonal-bank-198910/locations/us-central1/models/ICN4828686137575711807:predict -d ./public/uploads/request.json`, (err, stdout, stderr) => {
+        // if (err) {
+        //     return
+        // }
 
-    client.predict(request)
-    .then(responses => {
-        var response = responses[0];
-        res.send(response);
-    })
-    .catch(err => {
-        res.send(err)
-    });
+        console.log(stdout)
+        // spinner.stop();
+
+        // localList.push(outputParser(stdout))   
+
+        // loaded += 1
+    })   
+    // var client = new automl.v1beta1.PredictionServiceClient();
+    // var formattedName = client.modelPath(projectId, location, modelId);
+    // var payload = fs.readFileSync('./public/uploads/request.json')
+    // var request = {
+    //   name: formattedName,
+    //   payload: payload,
+    // };
+
+    // client.predict(request)
+    // .then(responses => {
+    //     var response = responses[0];
+    //     res.send(response);
+    // })
+    // .catch(err => {
+    //     res.send(err)
+    // });
 
 
     // let filePath = './public/uploads/335273.jpg'
