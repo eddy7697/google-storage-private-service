2a0bf893888830dbeac6059a9266f00f46bd50c6
李茗浤 <vincent7697@gmail.com>
2018-11-16 12:05:29 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -9,110 +9,114 @@ const storage = new Storage({
 
 const bucketName = 'nearlinetest-mark';
 
+router.get('/', (res, req, next) => {
+  res.send('GET request to the homepage');
+})
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const filePath = './uploads';  
-  const images = fs.readdirSync(filePath);  
+// router.get('/', function(req, res, next) {
+//   const filePath = './uploads';  
+//   const images = fs.readdirSync(filePath);  
 
-  const getFilesPromise = new Promise(function(resolve, reject) {
-      storage
-        .bucket(bucketName)
-        .getFiles()
-        .then(results => {
-          const files = results[0];
+//   const getFilesPromise = new Promise(function(resolve, reject) {
+//       storage
+//         .bucket(bucketName)
+//         .getFiles()
+//         .then(results => {
+//           const files = results[0];
 
-          resolve(files)
-        })
-        .catch(err => {
-          reject(err)
-        });      
-    });
+//           resolve(files)
+//         })
+//         .catch(err => {
+//           reject(err)
+//         });      
+//     });
   
-  const uploadFilesToStorage = new Promise((resolve, reject) => {
-    if (images.length > 0) {
-      var fileName = filePath + '/' + images[0];
+//   const uploadFilesToStorage = new Promise((resolve, reject) => {
+//     if (images.length > 0) {
+//       var fileName = filePath + '/' + images[0];
 
-      storage
-        .bucket(bucketName)
-        .upload(fileName, {
-          gzip: true,
-          metadata: {
-            cacheControl: 'public, max-age=31536000',
-          },
-        })
-        .then(() => {
-          fs.unlinkSync(fileName);
-          storage
-            .bucket(bucketName)
-            .getFiles()
-            .then(results => {
-              const files = results[0];
+//       storage
+//         .bucket(bucketName)
+//         .upload(fileName, {
+//           gzip: true,
+//           metadata: {
+//             cacheControl: 'public, max-age=31536000',
+//           },
+//         })
+//         .then(() => {
+//           fs.unlinkSync(fileName);
+//           storage
+//             .bucket(bucketName)
+//             .getFiles()
+//             .then(results => {
+//               const files = results[0];
     
-              resolve(files)
-            })
-            .catch(err => {
-              reject(err)
-            }); 
-        })
-        .catch(err => {
-          reject(err)
-          console.error('ERROR:', err);
-        });
-    } else {
-      storage
-        .bucket(bucketName)
-        .getFiles()
-        .then(results => {
-          const files = results[0];
+//               resolve(files)
+//             })
+//             .catch(err => {
+//               reject(err)
+//             }); 
+//         })
+//         .catch(err => {
+//           reject(err)
+//           console.error('ERROR:', err);
+//         });
+//     } else {
+//       storage
+//         .bucket(bucketName)
+//         .getFiles()
+//         .then(results => {
+//           const files = results[0];
 
-          resolve(files)
-        })
-        .catch(err => {
-          reject(err)
-        });  
-    }
+//           resolve(files)
+//         })
+//         .catch(err => {
+//           reject(err)
+//         });  
+//     }
     
-  })
+//   })
   
-  Promise.all([
-    uploadFilesToStorage
-  ]).then((results) => {
-    console.log(results[0])
-    res.render('index', { 
-      title: 'Express',
-      images: images ,
-      storageFiles: results[0]
-    });
-  }, (err) => {
-    console.log(err)
-    res.render('index', { 
-      title: 'Express',
-      images: images ,
-      storageFiles: []
-    });
-  });
+//   Promise.all([
+//     uploadFilesToStorage
+//   ]).then((results) => {
+//     console.log(results[0])
+//     res.render('index', { 
+//       title: 'Express',
+//       images: images ,
+//       storageFiles: results[0]
+//     });
+//   }, (err) => {
+//     console.log(err)
+//     res.render('index', { 
+//       title: 'Express',
+//       images: images ,
+//       storageFiles: []
+//     });
+//   });
   
-});
+// });
 
-function uploadFile(bucketName, filename) {
-  const Storage = require('@google-cloud/storage');
-  const storage = new Storage();
+// function uploadFile(bucketName, filename) {
+//   const Storage = require('@google-cloud/storage');
+//   const storage = new Storage();
   
-  storage
-    .bucket(bucketName)
-    .upload(filename, {
-      gzip: true,
-      metadata: {
-        cacheControl: 'public, max-age=31536000',
-      },
-    })
-    .then(() => {
-      console.log(`${filename} uploaded to ${bucketName}.`);
-    })
-    .catch(err => {
-      console.error('ERROR:', err);
-    });
-  // [END storage_upload_file]
-}
+//   storage
+//     .bucket(bucketName)
+//     .upload(filename, {
+//       gzip: true,
+//       metadata: {
+//         cacheControl: 'public, max-age=31536000',
+//       },
+//     })
+//     .then(() => {
+//       console.log(`${filename} uploaded to ${bucketName}.`);
+//     })
+//     .catch(err => {
+//       console.error('ERROR:', err);
+//     });
+//   // [END storage_upload_file]
+// }
 
 module.exports = router;
