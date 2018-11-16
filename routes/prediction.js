c92c2baf42123180303fa40ead2dd772eabfb946
李茗浤 <vincent7697@gmail.com>
2018-11-16 15:59:00 +0800

    update

--- routes/prediction.js
+++ routes/prediction.js
@@ -10,25 +10,45 @@ const modelId = 'ICN4828686137575711807'
 
 const bucketName = 'nearlinetest-mark';
 
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    let filePath = './public/uploads/335273.jpg'
-    let bitmap = fs.readFileSync(filePath);
-    let base64Encode
+    var client = new automl.v1beta1.PredictionServiceClient();
+    var formattedName = client.modelPath(projectId, location, modelId);
+    var payload = fs.readFileSync('./public/uploads/request.json')
+    var request = {
+      name: formattedName,
+      payload: payload,
+    };
+
+    client.predict(request)
+    .then(responses => {
+        var response = responses[0];
+        res.send(response);
+    })
+    .catch(err => {
+        res.send(err)
+    });
+
+
+    // let filePath = './public/uploads/335273.jpg'
+    // let bitmap = fs.readFileSync(filePath);
+    // let base64Encode
     
-    base64Encode = new Buffer(bitmap).toString('base64');
+    // base64Encode = new Buffer(bitmap).toString('base64');
 
-    let payload = {
-        "payload": {
-            "image": {
-                "imageBytes": base64Encode
-            },
-        }
-    }
+    // let payload = {
+    //     "payload": {
+    //         "image": {
+    //             "imageBytes": base64Encode
+    //         },
+    //     }
+    // }
 
-    fs.writeFileSync('./public/uploads/request.json', JSON.stringify(payload))
+    // fs.writeFileSync('./public/uploads/request.json', JSON.stringify(payload))
 
-    res.send(payload);
+    // res.send(payload);
 });
 
 module.exports = router;
