aac274022d85856a4693e4ebd5e6bd680222865a
Eddy <vincent7697@gmail.com>
2018-11-19 00:25:18 +0800

    test

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '30mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
