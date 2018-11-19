42d7c5ebbd5ab94f6a0d31a5d3c66670d79006e2
李茗浤 <vincent7697@gmail.com>
2018-11-19 14:56:52 +0800

    update images

--- routes/prediction.js
+++ routes/prediction.js
@@ -21,32 +21,17 @@ router.get('/:fileName', function(req, res, next) {
     let bitmap = fs.readFileSync(filePath);
     let imageBase64 = new Buffer(bitmap).toString('base64');
 
-    compress_images(filePath, srcPath, {
-        compress_force: false,
-        statistic: true,
-        autoupdate: true
-    }, false, {
-        jpg: {
-            'engine': 'mozjpeg',
-            'command': ['-quality', '60']
-        }
-    }, {
-        png: {
-            'engine': 'pngquant',
-            'command': ['--quality=20-50']
-        }
-    }, {
-        gif: {
-            'engine': 'gifsicle',
-            'command': ['--colors', '64', '--use-col=web']
-        }
-    }, function(error, completed, statistic) {
+    compress_images(filePath, srcPath, {compress_force: false, statistic: true, autoupdate: true}, false,
+        {jpg: {engine: 'mozjpeg', command: ['-quality', '60']}},
+        {png: {engine: 'pngquant', command: ['--quality=20-50']}},
+        {svg: {engine: 'svgo', command: '--multipass'}},
+        {gif: {engine: 'gifsicle', command: ['--colors', '64', '--use-col=web']}}, function(error, completed, statistic){
         console.log('-------------');
         console.log(error);
         console.log(completed);
         console.log(statistic);
-        console.log('-------------');
-    })
+        console.log('-------------');                                   
+    });
 
     // generate payload
     let payload = {
