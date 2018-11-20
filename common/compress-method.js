1ecff6851a50d8e0815ab728e66eb5df3bad9b2b
李茗浤 <vincent7697@gmail.com>
2018-11-20 11:38:50 +0800

    modufy compress rate to predict

--- common/compress-method.js
+++ common/compress-method.js
@@ -4,7 +4,7 @@ module.exports.image = image
 
 async function image(filePath, outputPath) {
     return compress_images(filePath, outputPath, {compress_force: false, statistic: true, autoupdate: true}, false,
-        {jpg: {engine: 'mozjpeg', command: ['-quality', '60']}},
+        {jpg: {engine: 'mozjpeg', command: ['-quality', '30']}},
         {png: {engine: 'pngquant', command: ['--quality=20-50']}},
         {svg: {engine: 'svgo', command: '--multipass'}},
         {gif: {engine: 'gifsicle', command: ['--colors', '64', '--use-col=web']}}, function(error, completed, statistic){
